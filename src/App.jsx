8573357292e8528370b883c4dcd7c0918ba01279
Ramya <muttireddyramya9@gmail.com>
2024-10-25 14:44:42 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AlertDialog  from './dialog'
 import ButtonAppBar from './appbar'
 import BasicSelect from './basicselect'
 import NestedList from './lists'
+import Typography from '@mui/material/Typography'
 import './App.css'
 
 function App() {
@@ -40,15 +41,18 @@ function App() {
           </Box>
           {/* Posts Section */}
           <Box sx={{ gridArea: 'Posts', bgcolor: 'white'}}>
-            <Box sx={{padding:"40px"}}>
+            <Box sx={{padding:"40px" ,display:"flex",flexDirection:"column" ,gap:"20px"}}>
+            <Typography varient="h3">Dialog Box</Typography>
             <AlertDialog />
               </Box>
           </Box>
 
           {/* Budgets Section */}
-          <Box sx={{ gridArea: 'Budgets', bgcolor: 'white' }}>
-          <Box sx={{padding:"24px"}}>
+          <Box sx={{ gridArea: 'Budgets', bgcolor: 'white',display:"flex",flexDirection:"column" ,gap:"100px"}}>
+          <Box sx={{padding:"24px" ,display:"flex",flexDirection:"column" ,gap:"10px"}}>
+          <Typography varient="h3">Select Items</Typography>
           <BasicSelect />
+          <Typography varient="h3">Avatar</Typography>
           <Grid container spacing={2} sx={{paddingTop:"30px"}}>
           <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
           <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
@@ -59,7 +63,8 @@ function App() {
 
           {/* Transctions Section */}
           <Box sx={{ gridArea: 'Tranctions', bgcolor: 'white' }}>
-          <Box sx={{padding:"40px"}}>
+          <Box sx={{padding:"40px" ,display:"flex",flexDirection:"column" ,gap:"20px"}}>
+          <Typography varient="h3">App Bar</Typography>
           <ButtonAppBar />
           </Box>
           </Box>
